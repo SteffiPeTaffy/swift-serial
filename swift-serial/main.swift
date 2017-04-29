95b0454b514e00a67162493bccd548726f8d5b97
Stefanie Grewenig <sgreweni@thoughtworks.com>
2017-04-29 18:23:28 +0200

    Initial Commit

---
+++ swift-serial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-serial
+//
+//  Created by Stefanie Grewenig on 29/04/2017.
+//  Copyright © 2017 Stefanie Grewenig. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
